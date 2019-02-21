f5a9a011a4b84f0259f2e90fd691c056ba855d10
Jos van Uden <josvanuden@gmail.com>
2019-02-21 01:09:40 +0100

    Make everything const until it hurts.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const a = [8,3,1,2,9,7,4,6,5,0];
 
 const insertionSort = (array) => {
-    
+
     for (let i = 1; i < array.length; i++) {
 
-        let key = array[i];
+        const key = array[i];
 
         let j = i - 1;
         for (; j >= 0 && array[j] > key; j--) {
